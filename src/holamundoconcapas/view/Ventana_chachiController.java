700efdb51375c16364022e79f5e8007b41c7edb7
Yeray <Yeray@DESKTOP-18VTL6K>
2019-09-25 14:14:25 +0200

    Probando el LOGGER con javaFx Javadoc añadido

--- src/holamundoconcapas/view/Ventana_chachiController.java
+++ src/holamundoconcapas/view/Ventana_chachiController.java
@@ -30,36 +30,51 @@ public class Ventana_chachiController{
     private String greeting;
     private Stage stage;
     
-    
+     private static final java.util.logging.Logger LOGGER=java.util.logging.Logger.getLogger("holamundoconcapas.view.Ventana_chachiController");
    
     @FXML
     public void buttonAction(ActionEvent event){
+        LOGGER.info("Start of the method buttonAction");
         texto.setText(greeting+greeting);
+        LOGGER.info("Finished the method buttonAction");
     }
 
     /**
-     *
+     *In thin method we set the text on the window when its shown
      * @param e
      */
     public void onWindowShowing(WindowEvent e){
+        LOGGER.info("Start of the method onWindowShowing");
     texto.setText(greeting);
+    LOGGER.info("Finished the method onWindowShowing");
     }
 
     /**
+     * We set the greeting
      * @param greeting the greeting to set
      */
     public void setGreeting(String greeting) {
+        
         this.greeting = greeting;
         
         
-    }
+    }/**
+     * the stage is initialized 
+     * @param root 
+     */
     public void initStage(Parent root){
+        LOGGER.info("Start of the method initStage");
          Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setOnShowing(this:: onWindowShowing);
         stage.show();
+        LOGGER.info("Finished the method initStage");
     }
 
+       /**
+        * We set the stage
+        * @param stage 
+        */
     void setStage(Stage stage) {
         this.stage=stage;
     }
